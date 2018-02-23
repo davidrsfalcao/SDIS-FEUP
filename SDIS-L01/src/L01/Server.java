acf43995e98d93bcc13ad4dcfffa1d91ef24e903
Diogopp <diogopeixotopereira@gmail.com>
2018-02-23 12:41:16 +0000

    Server almost done

--- SDIS-L01/src/L01/Server.java
+++ SDIS-L01/src/L01/Server.java
@@ -2,13 +2,19 @@ package L01;
 
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.HashMap;
 import java.net.DatagramPacket;
 import java.io.IOException;
 
 public class Server {
 	private static int port;
+	private static HashMap<String, String> plates;
+	
 	public static void main(String[] args) throws IOException {
-		port = Integer.parseInt(args[1]);
+		System.out.print("Entrei!\nl");
+		System.out.print(args[0]);
+		
+		port = Integer.parseInt(args[0]);
 		DatagramSocket socket = new DatagramSocket(port);
 		
 		byte[] buf = new byte[256];
@@ -17,13 +23,19 @@ public class Server {
 		
 		byte[] nbuf = packet.getData();
 		String dString = new String(nbuf);
-		/*
-		String dString = null;
-		if (in == null)
-		    dString = new Date().toString();
-		else
-		    dString = getNextQuote();
-		buf = dString.getBytes();*/
+		String[] arrayString = dString.split(":");
+		if (arrayString[0].compareTo("register") == 0 ) {
+			if (plates.containsKey(arrayString[1]))
+			plates.put(arrayString[1], arrayString[2]);
+		} else if (arrayString[0].compareTo("lookup") == 0) {
+			if (plates.containsKey(arrayString[1])) {
+				arrayString[2] = plates.get(arrayString[1]);
+				//response = arrayString[2];
+				System.out.print("Done!\nl");
+			}
+		}
+		
+		System.out.print(plates);
 		
 		InetAddress address = packet.getAddress();
 		int port = packet.getPort();
