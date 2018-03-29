4ba6c59e352abc96d084a224dfb442333039d0ba
Diogopp <diogopeixotopereira@gmail.com>
2018-03-29 16:17:50 +0100

    Add files via upload

--- Projeto/src/BackupProtocol.java
+++ Projeto/src/BackupProtocol.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 public class BackupProtocol implements Runnable {
     Peer peer;
     int replicationDegree;
+    byte[] chunk;
 
     public BackupProtocol(String version, String senderId, String path, int replicationDegree, Peer peer)  throws IOException, InterruptedException  {
         this.peer = peer;
@@ -15,6 +16,8 @@ public class BackupProtocol implements Runnable {
         /*String fileId = Utils.getFileId(new File(path));
         fileIds.put(path, fileId);
         requestsFileReplication.put(fileId, replicationDegree);*/
+
+        //CREATE THE CHUNK IN HERE//
 
         // TODO : Manager.backupFile(path, replicationDegree);
     }
