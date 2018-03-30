6f68186abf6a5b3605de48ad770aefbcdeeadf87
David Falcão <davidrsfalcao@gmail.com>
2018-03-30 12:49:25 +0100

    update

--- Projeto/src/server/Peer.java
+++ Projeto/src/server/Peer.java
@@ -1,58 +1,54 @@
 package server;
 
-import utils.RMI;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
+public class Peer implements RMI  {
 
-public class Peer{
+    public static int serverID;
+    private static Peer peer;
+    private static Thread backup;
+    private static Thread delete;
+    private static Thread restore;
+    private static Thread spaceReclaim;
 
-    private int serverID;
-    private int serviceAccessPoint;
+    private ConcurrentHashMap< String, String > chunksSaved;
+    private ConcurrentHashMap< String, String[] > initiatorVerifier;
 
     private String mcAddress;
     private int mcPort;
+    private static Thread mcChannel;
 
     private String mdbAddress;
     private int mdbPort;
+    private static Thread mdbChannel;
 
     private String mdrAddress;
     private int mdrPort;
 
-    public void setMdbPort(int mdbPort) {
-        this.mdbPort = mdbPort;
-    }
+    ///////////
+    private static int serviceAccessPoint;
 
-    public static void main(String[] args){
+    private static int MAXIMUM_SPACE = -1;
+    private static int USED_SPACE = 0;
+    ////////////////
 
-        Peer peer = new Peer();
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        peer = new Peer();
 
         if(!peer.validArgs(args))
             return;
 
         peer.initVars(args);
+        peer.openChannels();
 
-        ThreadPeer tread = new ThreadPeer(peer.serverID, peer.serviceAccessPoint, peer.mcAddress, peer.mcPort, peer.mdbAddress, peer.mdbPort, peer.mdrAddress, peer.mdrPort);
-        tread.start();
-
-        try {
-            RMI rmiObject = (RMI) UnicastRemoteObject.exportObject(tread, peer.serviceAccessPoint);
-            LocateRegistry.createRegistry(peer.serviceAccessPoint);
-            Registry registry = LocateRegistry.getRegistry();
-            registry.rebind(Integer.toString(peer.serverID), rmiObject);
-            System.err.println("Server ready");
-        } catch (Exception e) {
-            System.err.println("Server exception: " + e.toString());
-            e.printStackTrace();
-        }
-
+        //peer.startPeer();
     }
 
-
     private boolean validArgs(String[] args){
-        if (args.length != 8) {
-            System.out.println("Usage: java server.Peer <ServerID> <ServiceAccessPoint> <MC address> <MC port> <MDB address> <MDB port> <MDR address> <MDR port>");
+        if (args.length != 7) {
+            System.out.println("Usage: javan server.Peer  <ServerID> <MC address> <MC port> <MDB address> <MDB port> <MDR address> <MDR port>");
             return false;
         }
 
@@ -63,13 +59,141 @@ public class Peer{
         serverID = Integer.parseInt(args[0]);
 
         mcAddress = args[1];
-        mcPort = Integer.parseInt(args[2]);;
+        mcPort = Integer.parseInt(args[2]);
 
         mdbAddress = args[3];
-        mdbPort =Integer.parseInt(args[4]);;
+        mdbPort =Integer.parseInt(args[4]);
 
         mdrAddress = args[5];
-        mdrPort = Integer.parseInt(args[6]);;
+        mdrPort = Integer.parseInt(args[6]);
     }
 
+    private void openChannels() throws IOException, InterruptedException {
+        /*
+        try {
+            mcChannel = new Thread(new Mc(this.mcAddress, this.mcPort, this.peer));
+            mdbChannel = new Thread(new Mdb(this.mcAddress, this.mcPort, this.peer));
+
+            mcChannel.start();
+            mdbChannel.start();
+        }
+        catch (IOException error) {
+            System.out.println("Couldn't create the channels!");
+        }
+        */
+    }
+
+    /*private void startPeer() throws IOException, InterruptedException {
+        String read = new String();
+        while (read != "end") {
+            read = this.peer.receiveProtocol();
+            this.peer.startProtocol(read);
+        }
+    }*/
+
+    private String receiveProtocol() {
+        return null;
+    }
+
+    /*private void startProtocol(String read) throws IOException, InterruptedException {
+        String[] control = read.split(" ");
+        if (control[0] == "Backup") {
+            try {
+                backup = new Thread(new BackupProtocol(control, this.peer));
+                backup.start();
+            }
+            catch (IOException error) {
+                System.out.println("Couldn't create the backupProtocol!");
+            }
+        } else if (control[0] == "Delete") {
+            try {
+                backup = new Thread(new DeleteProtocol(control, this.peer));
+                backup.start();
+            }
+            catch (IOException error) {
+                System.out.println("Couldn't create the backupProtocol!");
+            }
+        } else if (control[0] == "Restore") {
+            try {
+                backup = new Thread(new RestoreProtocol(control, this.peer));
+                backup.start();
+            }
+            catch (IOException error) {
+                System.out.println("Couldn't create the backupProtocol!");
+            }
+        } else if (control[0] == "SpaceReclaim") {
+            try {
+                backup = new Thread(new SReclaimProtocol(control, this.peer));
+                backup.start();
+            }
+            catch (IOException error) {
+                System.out.println("Couldn't create the backupProtocol!");
+            }
+        }
+    }*/
+
+    @Override
+    public void backup(String version, String senderId, String path, int replicationDegree) {
+        /*
+        try {
+            backup = new Thread(new BackupProtocol(version, senderId, path, replicationDegree, this.peer));
+            backup.start();
+        }
+        catch (IOException error) {
+            System.out.println("Couldn't create the backupProtocol!");
+        }
+        catch (InterruptedException error) {
+            System.out.println("Interrupted!");
+        }
+        */
+    }
+
+    @Override
+    public void restore(String version, String senderId, String path) {
+/*        try {
+            restore = new Thread(new RestoreProtocol(version, senderId, path, this.peer));
+            restore.start();
+        }
+        catch (IOException error) {
+            System.out.println("Couldn't create the backupProtocol!");
+        }
+        catch (InterruptedException error) {
+            System.out.println("Interrupted!");
+        }*/
+    }
+
+    @Override
+    public void delete(String version, String senderId, String path) {
+/*        try {
+            delete = new Thread(new DeleteProtocol(version, senderId, path, this.peer));
+            delete.start();
+        }
+        catch (IOException error) {
+            System.out.println("Couldn't create the backupProtocol!");
+        }
+        catch (InterruptedException error) {
+            System.out.println("Interrupted!");
+        }*/
+    }
+
+    @Override
+    public void reclaim(String version, String senderId, int space) {
+        /*
+        try {
+            spaceReclaim = new Thread(new SReclaimProtocol(version, senderId, space, this.peer));
+            spaceReclaim.start();
+        }
+        catch (IOException error) {
+            System.out.println("Couldn't create the backupProtocol!");
+        }
+        catch (InterruptedException error) {
+            System.out.println("Interrupted!");
+        }
+        */
+    }
+
+    @Override
+    public String state() {
+        return null;
+    }
 }
